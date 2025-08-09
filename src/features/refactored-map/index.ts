a6276ecf788f05ce2713255ec65fe1150a80b36d
Rob Jacobson <rljacobson@outlook.com>
2025-08-09 11:42:57 -0700

    Update exports in refactored-map index to include DebugPanel and reposition MainMap
    - Added DebugPanel component export for enhanced map state visibility.
    - Moved MainMap export to the end of the index file for better organization.

--- src/features/refactored-map/index.ts
+++ src/features/refactored-map/index.ts
@@ -1,6 +1,7 @@
-export { MainMap } from "./components/MainMap";
+export { DebugPanel } from "./components/DebugPanel";
 export { MapComponent } from "./components/MapComponent";
 export { RoutesLayer } from "./components/RoutesLayer";
 export { VesselLayer } from "./components/VesselLayer";
 export { VesselLines } from "./components/VesselLines";
 export { VesselMarkers } from "./components/VesselMarkers";
+export { MainMap } from "./MainMap";
